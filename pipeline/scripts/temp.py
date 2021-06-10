1f003b41fcadda336a65ed93acd27644ed3fb1e8
gyntera1 <artur.gynter@aalto.fi>
2021-06-10 12:58:53 +0300

    cleaning code and proper argument parsing

--- pipeline/scripts/temp.py
+++ pipeline/scripts/temp.py
@@ -1,17 +1,45 @@
-import sys
-from os.path import expanduser
-import subprocess as sp
-from ete3 import Tree, NCBITaxa
+import argparse
 import time
-ncbi = NCBITaxa()
-
-
-
-def get_highest_taxonomic_sequence(ids):
-    tax_tree = ncbi.get_topology(ids)
-    for node in tax_tree.traverse("levelorder"):
-        print(node)
 
 
 def get_time_ms():
     return round(time.time() * 1000)
+
+
+def main(args):
+    if args.action == "a":
+        pass
+    elif args.action == "i":
+        pass
+    elif args.action == "p":
+        pass
+    elif args.action == "w":
+        pass
+    else:
+        pass
+# Returns true if arguments are as required
+def check_args(parser, args):
+    if args.action == "a":
+        return True
+    elif args.action == "i":
+        return True
+    elif args.action == "p":
+        parser.error("Action \"p\" requires --id")
+        return "id" in vars(args)
+    elif args.action == "w":
+        parser.error("Action \"w\" requires --id")
+        return "id" in vars(args)
+    else:
+        return False
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("action", type=str, help="a/i/p/w")
+    parser.add_argument("db", type=str, help="database in fasta-format")
+    parser.add_argument("clusters", type=str, help="cluster file")
+    parser.add_argument("--min", type=int, default=10, help="minimum size of the cluster {10}")
+    parser.add_argument("--max", type=int, default=1000, help="maximum size of the cluster {1000}")
+    parser.add_argument("--id", type=str, help="cluster id to separate")
+    args = parser.parse_args()
+    if check_args(parser, args):
+        main(args)
