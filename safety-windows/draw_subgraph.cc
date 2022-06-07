dd3a9c6cebb7a1b5d0836818b64c6f4c51a6661c
Grigorjew Andreas <andrgrig@dx1-523-001.pc.helsinki.fi>
2022-06-07 15:18:57 +0300

    Small output changes and more polished draw subgraph routine

--- safety-windows/draw_subgraph.cc
+++ safety-windows/draw_subgraph.cc
@@ -4,10 +4,32 @@
 #include <vector>
 #include "draw_subgraph.h"
 
-std::string draw_subgraph(const int64_t IDX, const int64_t n, const int64_t m, const Dag &d, const std::vector<int64_t> &alpha_path, const std::vector<std::pair<int64_t, int64_t>> &windows) {
+std::string draw_subgraph(const int64_t IDX, const int64_t n, const int64_t m, const Dag &d, const std::vector<int64_t> &alpha_path, const std::vector<std::pair<int64_t, int64_t>> &windows, const std::string &a, const std::string &b) {
+	std::unordered_map<int64_t, int64_t> order;
+	for (int64_t i = 0; i < (int) alpha_path.size(); i++) order[alpha_path[i]] = i;
+
 	auto has = [&](int64_t i, int64_t j) {
 		// check if pair (i, j) is part of the suboptimal dag
 		return d.trans.find(std::make_pair(i, j)) != d.trans.end();
+	};
+	auto is_safe = [&](int64_t i1, int64_t j1, int64_t i2, int64_t j2) {
+		if (!has(i1, j1) || !has(i2, j2)) return 0;
+		int64_t a1 = -1, a2 = -1;
+		for (int64_t a: alpha_path) {
+			if (d.transr.at(a) == std::make_pair(i1, j1)) a1 = a;
+			if (d.transr.at(a) == std::make_pair(i2, j2)) a2 = a;
+		}
+		if (a1 < 0 || a2 < 0) return 0;
+		if (order[a1] > order[a2]) {
+			std::cerr << "DBG: " << i1 << ' ' << j1 << ' ' << i2 << ' ' << j2 << ' ' << a1 << ' ' << a2 << std::endl;
+			assert(false);
+		}
+		int idx = 1;
+		for (auto [L, R]: windows) {
+			if (order[a1] >= order[L] && order[a1] <= order[R] && order[a2] >= order[L] && order[a2] <= order[R]) return idx;
+			idx++;
+		}
+		return 0;
 	};
 	auto outside = [&](int64_t i, int64_t j) {
 		return i < 0 || i >= n || j < 0 || j >= m;
@@ -18,11 +40,16 @@ std::string draw_subgraph(const int64_t IDX, const int64_t n, const int64_t m, c
 		for (int64_t a: as) if (std::find(alpha_path.begin(), alpha_path.end(), a) != alpha_path.end()) return true;
 		return false;
 	};
-	std::string output = "graph fasta_" + std::to_string(IDX) + " {\nnode [shape=none];\n";//rankdir=LR;\n";
+	std::string output = "graph fasta_" + std::to_string(IDX) + " {\nnode [shape=none, width=.2, height=.05, fixedsize=true];\n";//rankdir=LR;\n";
 	std::vector<std::string> nodes, edges;
 	for (int64_t i = 0; i < n; i++) {
 		for (int64_t j = 0; j < m; j++) {
-			std::string node = "\"" + std::to_string(i) + "_" + std::to_string(j) + "\" [label=\"" + std::to_string(i) + "_" + std::to_string(j) + "\", style=invis]";
+			std::string node_label;
+			if (i == 0 && j > 0) node_label = std::string(1, b[j - 1]);
+			else if (i > 0 && j == 0) node_label = std::string(1, a[i - 1]);
+			else node_label = std::to_string(i) + "_" + std::to_string(j);
+			std::string node = "\"" + std::to_string(i) + "_" + std::to_string(j) + "\" [label=\"" + node_label + "\"";
+			if ((i > 0 && j > 0) || (i == 0 && j == 0)) node += ", style=invis]"; else node += "]";
 			nodes.push_back(node);
 
 			std::vector<std::pair<int64_t, int64_t>> from = { {i - 1, j - 1}, {i - 1, j}, {i, j - 1} };
@@ -31,8 +58,12 @@ std::string draw_subgraph(const int64_t IDX, const int64_t n, const int64_t m, c
 				if (!has(i, j) || !has(l, k)) {
 					edge += " [style=invis]";
 					assert(!in_alpha_path(i, j) || !in_alpha_path(l, k));
+				} else {
+					int s;
+					if ((s = is_safe(l, k, i, j))) edge += ((s % 2) ? " [color=red, penwidth=3]" : " [color=purple, penwidth=3]");
+					else if (in_alpha_path(i, j) && in_alpha_path(l, k)) edge += " [color=blue, penwidth=3]";
+					else edge += " [style=bold]";
 				}
-				else if (in_alpha_path(i, j) && in_alpha_path(l, k)) edge += " [color=blue]"; // this can be made faster easily if needed
 				edges.push_back(edge);
 			}
 		}
